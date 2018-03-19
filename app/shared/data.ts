72089063b86fcc16c218290ae35136c45fa19855
brother <ruslan.brother@gmail.com>
2018-03-19 17:54:01 +0200

    getting started

---
+++ app/shared/data.ts
@@ -0,0 +1,11 @@
+export const todos = [
+	{	title: 'Изучить JS',
+		completed: true
+	},
+	{	title: 'Изучить A2',
+		completed: true
+	},
+	{	title: 'Написать приложуху',
+		completed: false
+	}
+];
